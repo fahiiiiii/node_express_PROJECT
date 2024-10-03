1145d88cb328a95a7c7f560ae2e562ac2093c0cd
Fahimah <fahimah315@gmail.com>
2024-10-04 01:46:07 +0600

    file uploaded using multer

--- task_manager/server.js
+++ task_manager/server.js
@@ -1,19 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const connectDB = require("./config/db");
 app.use(bodyParser.json());
-require('dotenv').config();
+require("dotenv").config();
 
 connectDB();
 
-app.use('/users',require('./routes/api/users'));
-app.use('/tasks',require('./routes/api/tasks'));
+app.use("/users", require("./routes/api/users"));
+app.use("/tasks", require("./routes/api/tasks"));
 
-app.get('/',(req,res)=>{
-    res.json({Message:"Welcome to the User app"})
-})
+app.get("/", (req, res) => {
+  res.json({ Message: "Welcome to the task-manager app" });
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const fileName  = file.originalname.split('.')[0]+'-'+file.fieldname + '-' + uniqueSuffix + '-' + file.originalname.split('.')[1]
+      //  cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
+       cb(null, fileName)
+    }
+  })
+
+  const upload = multer({ storage: storage })
+
+// const upload = multer({ dest: "./public/uploads/" });
+app.post("/uploads", upload.single("file"),  (req, res)=> {
+  res.json({ Message: "your file uploaded successfully" });
+});
+
+
+
+
 const port = process.env.PORT;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
